bcc7d8348cfe4b31893551f6a5c17f86b8e2a727
Zakodzony <32582050+Zakodzony@users.noreply.github.com>
2017-11-04 10:59:17 +0100

    Update lab3_zad3.c
    Poprawki w obsludze bledow

--- Lab3/Lab3_zad3/lab3_zad3.c
+++ Lab3/Lab3_zad3/lab3_zad3.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <pwd.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 void show_stat_info(char *fname, struct stat *buf);
 void mode_to_letters(int mode, char *str);
@@ -11,67 +12,81 @@ char tryby[15];
 
 int main(int argc, char *argv[])
 {
-    struct stat info;   
-	int opcja;
-	opcja = getopt(argc, argv, ":r:w:x:");
+    struct stat info;
+    if(argc == 3)
+    {
+        int opcja;
+        opcja = getopt(argc, argv, ":r:w:x:");
 
-	if (opcja == -1)
-	{
-        perror("Nie podano opcji");
-		return(1);
-	}
-
-	stat(argv[2], &info);
-	show_stat_info( argv[1], &info );
-	switch (opcja)
-	{
-		case 'r':
-            printf("Opcja czytania:\n");
-            printf("Wlasciciel:\t"); (tryby[1]=='r') ? printf("tak\n") : printf("nie\n");
-            printf("Grupa:\t\t"); (tryby[4]=='r') ? printf("tak\n") : printf("nie\n");
-            printf("Pozostali:\t"); (tryby[7]=='r') ? printf("tak\n") : printf("nie\n");
-			break;
-		case 'w':
-            printf("Opcja zapisu:\n");
-			printf("Wlasciciel:\t"); (tryby[2]=='w') ? printf("tak\n") : printf("nie\n");
-            printf("Grupa:\t\t"); (tryby[5]=='w') ? printf("tak\n") : printf("nie\n");
-            printf("Pozostali:\t"); (tryby[8]=='w') ? printf("tak\n") : printf("nie\n");
-			break;
-		case 'x':
-            printf("Opcja wykonania:\n");
-			printf("Wlasciciel:\t"); (tryby[3]=='x') ? printf("tak\n") : printf("nie\n");
-            printf("Grupa:\t\t"); (tryby[6]=='x') ? printf("tak\n") : printf("nie\n");
-            printf("Pozostali:\t"); (tryby[9]=='x') ? printf("tak\n") : printf("nie\n");
-			break;
-		default:
-			return(1);
-	}
-  return 0;
+        if (opcja == -1)
+        {
+            perror("getopt");
+            exit(1);
+        }
+        
+        if(stat(argv[2], &info) != 0)
+        {
+            perror("stat");
+            exit(1);
+        }
+            
+        show_stat_info( argv[1], &info );
+        switch (opcja)
+        {
+            case 'r':
+                printf("Opcja czytania:\n");
+                printf("Wlasciciel:\t"); (tryby[1]=='r') ? printf("tak\n") : printf("nie\n");
+                printf("Grupa:\t\t"); (tryby[4]=='r') ? printf("tak\n") : printf("nie\n");
+                printf("Pozostali:\t"); (tryby[7]=='r') ? printf("tak\n") : printf("nie\n");
+                break;
+            case 'w':
+                printf("Opcja zapisu:\n");
+                printf("Wlasciciel:\t"); (tryby[2]=='w') ? printf("tak\n") : printf("nie\n");
+                printf("Grupa:\t\t"); (tryby[5]=='w') ? printf("tak\n") : printf("nie\n");
+                printf("Pozostali:\t"); (tryby[8]=='w') ? printf("tak\n") : printf("nie\n");
+                break;
+            case 'x':
+                printf("Opcja wykonania:\n");
+                printf("Wlasciciel:\t"); (tryby[3]=='x') ? printf("tak\n") : printf("nie\n");
+                printf("Grupa:\t\t"); (tryby[6]=='x') ? printf("tak\n") : printf("nie\n");
+                printf("Pozostali:\t"); (tryby[9]=='x') ? printf("tak\n") : printf("nie\n");
+                break;
+            default:
+                return(1);
+        }
+        return 0;
+    }
+    else
+    {
+        printf("Niewlasciwa liczba argumentow. Poprawne wywolanie: %s -[r/w/x] [nazwa_pliku]\n", argv[0]);
+        exit(1);
+    }
 }
 
 void show_stat_info(char *fname, struct stat *buf)
 {
     mode_to_letters(buf->st_mode,tryby);
- 
 }
 
 void mode_to_letters(int mode, char str[])
 {
-	strcpy(str,"----------");
+        strcpy(str,"----------");
 
-	if(S_ISDIR(mode) ) str[0] = 'd';
-	if(S_ISCHR(mode) ) str[0] = 'c';
-	if(S_ISBLK(mode) ) str[0] = 'b';
+        if(S_ISDIR(mode) ) str[0] = 'd';
+        if(S_ISCHR(mode) ) str[0] = 'c';
+        if(S_ISBLK(mode) ) str[0] = 'b';
 
-	if(mode & S_IRUSR ) str[1] = 'r'; 
-	if(mode & S_IWUSR ) str[2] = 'w';
-	if(mode & S_IXUSR ) str[3] = 'x';
+        if(mode & S_IRUSR ) str[1] = 'r'; 
+        if(mode & S_IWUSR ) str[2] = 'w';
+        if(mode & S_IXUSR ) str[3] = 'x';
 
-	if(mode & S_IRGRP ) str[4] = 'r';
-	if(mode & S_IWGRP ) str[5] = 'w';
-	if(mode & S_IXGRP ) str[6] = 'x';
+        if(mode & S_IRGRP ) str[4] = 'r';
+        if(mode & S_IWGRP ) str[5] = 'w';
+        if(mode & S_IXGRP ) str[6] = 'x';
 
-	if(mode & S_IROTH ) str[7] = 'r';
-	if(mode & S_IWOTH ) str[8] = 'w';
-	if(mode & S_IXOTH ) str[9] = 'x';
+        if(mode & S_IROTH ) str[7] = 'r';
+        if(mode & S_IWOTH ) str[8] = 'w';
+        if(mode & S_IXOTH ) str[9] = 'x';
 } 
+
+
